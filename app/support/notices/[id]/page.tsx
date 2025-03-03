28ebb3c31890048a61a5d2cba508fa8d09250019
minseochh02 <minseochh02@gmail.com>
2025-03-03 16:41:01 +0900

    fix type errors

--- app/support/notices/[id]/page.tsx
+++ app/support/notices/[id]/page.tsx
@@ -1,17 +1,13 @@
 import { NoticeDetailContent } from "./NoticeDetailContent";
 
-type Props = {
-	params: {
-		id: string;
-	};
-	searchParams: { [key: string]: string | string[] | undefined };
-};
-
 export const metadata = {
 	title: "공지사항 상세 - SanggaTalk",
 	description: "SanggaTalk의 공지사항을 확인하세요.",
 };
 
-export default function NoticeDetailPage({ params }: Props) {
+type Params = Promise<{ id: string }>;
+
+export default async function NoticeDetailPage(props: { params: Params }) {
+	const params = await props.params;
 	return <NoticeDetailContent noticeId={parseInt(params.id)} />;
 }
