71312a05449b0ebdc8ff4a9de34d1b27a223388f
minseochh02 <minseochh02@gmail.com>
2025-03-11 15:29:40 +0900

    seeing if its returning too early

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,6 +3,7 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
