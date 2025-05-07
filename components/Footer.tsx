34af60d06df893cb693b33c3cdf5957b87711524
Minseo Cha <minseocha@Minseos-MacBook-Pro.local>
2025-05-07 13:42:57 +0900

    added privacy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,15 +3,21 @@ import Link from "next/link";
 export default function Footer() {
 	return (
 		<footer className="w-full flex items-center justify-center border-t border-t-foreground/10 py-16">
-			<div className="max-w-7xl w-full flex flex-col md:flex-row justify-between items-center px-5 text-sm gap-4">
-				<p>© 2024 SGTWallet. All rights reserved.</p>
-				<div className="flex gap-4">
-					<Link href="/privacy" className="hover:text-primary">
-						Privacy Policy
-					</Link>
-					<Link href="/terms" className="hover:text-primary">
-						Terms of Service
-					</Link>
+			<div className="max-w-7xl w-full flex flex-col items-center px-5 text-sm gap-4">
+				<div className="flex flex-col md:flex-row justify-between w-full items-center gap-4">
+					<p>© 2024 (주)쿠스. All rights reserved.</p>
+					<div className="flex gap-4">
+						<Link href="/privacy" className="hover:text-primary">
+							Privacy Policy
+						</Link>
+						<Link href="/terms" className="hover:text-primary">
+							Terms of Service
+						</Link>
+					</div>
+				</div>
+				<div className="text-xs text-gray-500 text-center mt-2">
+					<p>사업자등록번호: 731-81-02023</p>
+					<p>주소: 경기도 시흥시 서울대학로 59-69 배곧테크노밸리 609호 (우편번호: 15012)</p>
 				</div>
 			</div>
 		</footer>
